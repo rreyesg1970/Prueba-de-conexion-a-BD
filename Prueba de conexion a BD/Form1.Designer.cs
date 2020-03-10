df4e105eba616410e49d5a820f709ca6d538791c
Roberto Reyes G <r_reyes98@hotmail.com>
2020-03-09 20:51:16 -0600

    Agregar archivos de proyecto.

---
+++ Prueba de conexion a BD/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Prueba_de_conexion_a_BD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Obtener Datos SQL Server";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 39);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(740, 173);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(764, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Prueba de conexion a BD/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prueba_de_conexion_a_BD
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection Conexion = new SqlConnection("Server=.;DataBase = System;Integrated Security = true");
+        private SqlCommand cmd; 
+        private DataTable dt = new DataTable();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MostrarProductos();
+            CerrarConexion();
+        }
+
+        public void MostrarProductos()
+        {
+            DataTable dt = MostrarTodosProductos();
+            dataGridView1.DataSource = dt;
+            dataGridView1.Columns["Costo"].DefaultCellStyle.Format = "N2";
+            dataGridView1.Columns["PrecioVenta1"].DefaultCellStyle.Format = "N2";
+            dataGridView1.Columns["PrecioVenta2"].DefaultCellStyle.Format = "N2";
+            dataGridView1.Columns["PrecioVenta3"].DefaultCellStyle.Format = "N2";
+            dataGridView1.Columns["PrecioVenta4"].DefaultCellStyle.Format = "N2";
+            dataGridView1.Columns["PrecioVenta5"].DefaultCellStyle.Format = "N2";
+        }
+
+        public DataTable MostrarTodosProductos()
+        {
+            cmd = new SqlCommand("SP_MostrarTodosProductos", Conexion);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            return dt;
+        }
+
+        public SqlConnection AbrirConexion()
+        {
+            if (Conexion.State == ConnectionState.Closed)
+                Conexion.Open();
+            return Conexion;
+        }
+
+        public SqlConnection CerrarConexion()
+        {
+            if (Conexion.State == ConnectionState.Open)
+                Conexion.Close();
+            return Conexion;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            CerrarConexion();
+        }
+    }
+}
